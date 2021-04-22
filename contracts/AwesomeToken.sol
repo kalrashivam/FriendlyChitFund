38e84cbb56d90680269bd4e2bca5b8e93f6252ac
kalrashivam <shivamkalra2017@gmail.com>
2021-04-22 20:41:30 +0530

    add transfer functionality

--- contracts/AwesomeToken.sol
+++ contracts/AwesomeToken.sol
@@ -2,8 +2,12 @@
 pragma solidity >=0.7.0 <=0.9.0;
 
 contract AwesomeToken {
-    event Transfer(address indexed _from, address indexed _to, uint256 _value)
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+    event Transfer(address indexed _from,
+                   address indexed _to,
+                   uint256 _value);
+    event Approval(address indexed _owner,
+                   address indexed _spender,
+                   uint256 _value);
 
     string tokenSymbol = 'AWT';
     string tokenName = 'AwesomeTOKEN';
@@ -25,5 +29,13 @@ contract AwesomeToken {
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
         balance = ownerBalance[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(ownerBalance[msg.sender] >= _value);
+        ownerBalance[msg.sender] -= _value;
+        ownerBalance[_to] = _value;
+        emit Transfer(msg.sender, _to, _value);
+        success = True;
     }
 }
