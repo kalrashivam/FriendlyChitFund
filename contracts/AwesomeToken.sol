9ae8a7f753809c4e8210ac4d1ee47bbfcd863f94
kalrashivam <shivamkalra2017@gmail.com>
2021-04-24 00:53:01 +0530

    Add replica of hook and burn, mint capabilities

--- contracts/AwesomeToken.sol
+++ contracts/AwesomeToken.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: <SPDX-License>
+//SPDX-License-Identifier:  UNLICENSED
 pragma solidity >=0.7.0 <=0.9.0;
 
 contract AwesomeToken {
@@ -9,23 +9,38 @@ contract AwesomeToken {
                    address indexed _spender,
                    uint256 _value);
 
-    string tokenSymbol = 'AWT';
-    string tokenName = 'AwesomeTOKEN';
-    uint tokenSupply = 10000;
+    string private _tokenSymbol;
+    string private _tokenName;
+    uint private _tokenSupply;
+    uint128 private _mintCount;
+    uint128 private _burnCount;
+    uint128 private _transferCount;
 
     mapping(address => uint) ownerBalance;
     mapping(address => mapping(address => uint)) approvedAmount;
 
+    constructor() {
+        _tokenSymbol = 'AWT';
+        _tokenName = 'AwesomeTOKEN';
+        _tokenSupply = 10000;
+    }
+
+    // // Test function: Wouldn't exist in the actual contract
+    function addtesttoken(address _to) public returns (bool) {
+        ownerBalance[_to] = 5;
+        return true;
+    }
+
     function name() public view returns (string memory) {
-        return tokenName;
+        return _tokenName;
     }
 
     function symbol() public view returns (string memory) {
-        return tokenSymbol;
+        return _tokenSymbol;
     }
 
     function totalSupply() public view returns (uint256) {
-        return tokenSupply;
+        return _tokenSupply;
     }
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
@@ -33,6 +48,8 @@ contract AwesomeToken {
     }
 
     function _transfer(address _from, address _to, uint _value) private returns (bool) {
+        _beforeTokenTransfer(_from, _to, _value);
+
         ownerBalance[_from] -= _value;
         ownerBalance[_to] = _value;
         emit Transfer(_from, _to, _value);
@@ -58,5 +75,38 @@ contract AwesomeToken {
         approvedAmount[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         success = true;
+    }
+
+    function burn(address account, uint256 amount) public returns (bool success) {
+        _beforeTokenTransfer(account, address(0), amount);
+        uint balance = ownerBalance[account];
+        require(balance >= amount);
+        ownerBalance[account] -= amount;
+        _tokenSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+        success = true;
+    }
+
+    function mint(address account, uint256 amount) public returns (bool success) {
+        _beforeTokenTransfer(address(0), account, amount);
+        require(amount <= _tokenSupply);
+        _tokenSupply += amount;
+        ownerBalance[account] += amount;
+
+        emit Transfer(address(0), account, amount);
+        success = true;
+    }
+
+    function _beforeTokenTransfer(address _from, address _to, uint256 amount) private returns (bool) {
+        if (_from == address(0)) {
+            _mintCount += 1;
+        } else if (_to == address(0)) {
+            _burnCount += 1;
+        } else {
+            _transferCount += 1;
+        }
+
+        return true;
     }
 }
