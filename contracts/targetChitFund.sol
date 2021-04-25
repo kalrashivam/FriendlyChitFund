77610cfef35d30faffafd5580ed2569314485cb3
kalrashivam <shivamkalra2017@gmail.com>
2021-04-25 23:27:13 +0530

    minor changes

--- contracts/targetChitFund.sol
+++ contracts/targetChitFund.sol
@@ -5,44 +5,51 @@ import {IERC20} from "./erc20Interface.sol";
 import "hardhat/console.sol";
 
 contract TargetChitFund {
-    struct Member {
+    struct Pool {
         uint targetAmount;
         uint maxMembers;
-        uint currentMembers;
     }
 
     Pool[] public pools;
-    mapping (address => uint) poolId;
-    mapping (address => uint) userAmount;
+    mapping(address => uint) poolId;
+    mapping(address => uint) userAmount;
     // address TokenAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     // IERC20 dieToken = IERC20(TokenAddress);
 
-    function createPool(address _owner, uint _amount, uint _members) external returns (uint) {
-        pools.push(Pool(_amount, _members, 1));
-        uint id = pools.length - 1;
-        poolId[_owner] = id;
-        return id;
-    }
-
-    function joinPool(address account, uint _poolId) external returns (bool success) {
-        require(pools[_poolId].currentMembers < pools[_poolId].maxMembers);
-
-        poolId[account] = _poolId;
-        pools[_poolId].currentMembers += 1;
-        success = true;
-    }
-
-    function depositAmount(address account, uint amount) external returns (bool) {
-        userAmount[account] += amount;
-        if (userAmount[account] >= pools[poolId[account]].targetAmount) {
-            // Aave call
-            return true;
-        }
-
+    function _deposit(address _account, uint _amount) private returns (bool) {
+        userAmount[_account] += _amount;
+        // Move token from address to aave
         return true;
     }
 
-    function claimAmount(address account, uint amount) external returns (bool) {
-        userAmount[account] = 0;
+    function _withdraw(address _account) private returns (bool) {
+        // Move token from aave to address
+        delete poolId[_account];
+        userAmount[_account] = 0;
+        return true;
+    }
+
+    function createPool(address _owner, uint _amount, uint _members) public returns (bool) {
+        pools.push(Pool(_amount, _members));
+        poolId[_owner] = pools.length - 1;
+        return true;
+    }
+
+    function joinPool(address _account, uint _poolId) external returns (bool) {
+        require(pools.length - 1 <= _poolId, "Pool does not exist");
+        poolId[_account] = _poolId;
+        return true;
+    }
+
+    function depositAmount(address _account, uint _amount) external returns (bool) {
+        require(poolId[_account] >= 0, "Account not part of a pool.");
+
+        if ((userAmount[_account] + _amount) >= pools[poolId[_account]].targetAmount) {
+            _withdraw(_account);
+        } else {
+            _deposit(_account, _amount);
+        }
+
+        return true;
     }
 }
--- test/chitfund-test.js
+++ test/chitfund-test.js
@@ -2,6 +2,15 @@ const { expect } = require("chai");
 
 describe("TargetChitFund", function() {
     it("Trial Testing", async function() {
-        
+        const TargetChitFund = await ethers.getContractFactory("TargetChitFund");
+        const tcf = await TargetChitFund.deploy();
+
+        await tcf.deployed();
+
+        // Fetchoing addresses
+        const [owner, addr1, addr2] = await ethers.getSigners();
+
+        await tcf.createPool(owner.address, 50, 5);
+        await tcf.depositAmount(owner.address, 5, 2);
     });
 });
